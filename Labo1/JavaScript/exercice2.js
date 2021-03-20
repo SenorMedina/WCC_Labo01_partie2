1002d731f91645261e4bda82dee6214810139abe
Monark-Dev <78184002+Monark-Dev@users.noreply.github.com>
2021-03-19 22:04:05 -0400

    Update exercice2.js

--- Labo1/JavaScript/exercice2.js
+++ Labo1/JavaScript/exercice2.js
@@ -21,3 +21,31 @@ function verif(){
     var resultatDuCalcul= eval(calcul); 
     document.getElementById("message").value = resultatDuCalcul;
 }
+
+
+// ajout pour exercice 3
+
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min) + min);
+}
+nombreSecret = getRandomInt(1, 100);
+
+tentative;
+const maxEsssais = 7;
+essais = 1;
+
+function valider() {
+    while (essais <= maxEsssais) {
+        if (nombreSecret > tentative) {
+            message = "C'est plus!";
+        } else if (nombreSecret < guess) {
+            message = "c'est moins";
+        }else{
+            message = "Vous avez trouvé en " + essais + " essais";
+        }
+        essais++
+    }
+    message = "Le nombre à touver était : " + nombreSecret;
+}
